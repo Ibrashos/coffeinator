a3197b717fa35e90c2df3227d307b712752851b2
aliradjabov <baryalin100@gmail.com>
2023-05-20 16:53:24 +0300

    добавлен файл ChatGPT.py

---
+++ ChatGPT.py
@@ -0,0 +1,21 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("TOKEN_GPT")
+
+
+def chat(text: str, token: str = openai.api_key):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"{text}",
+        temperature=0.5,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0.5,
+        presence_penalty=0)
+    return response['choices'][0]['text']
+
+
+print(chat(text="hello"))
